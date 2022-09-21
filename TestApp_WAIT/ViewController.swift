ff066cb5170a2e8cc672cc6d7754c49a9b13312b
OdayMohamed <odaydieg@gmail.com>
2022-09-21 19:39:57 +0200

    Initial Commit

---
+++ TestApp_WAIT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestApp_WAIT
+//
+//  Created by Oday Dieg on 21/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
